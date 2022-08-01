a63ce2ad1165dd11638fac85065efb076af7ee49
Marcin <mlojek@protonmail.com>
2022-08-01 18:33:42 +0200

    add get_card_image function

--- prodeck_api.py
+++ prodeck_api.py
@@ -1,6 +1,20 @@
 import json
+import os
 
 import requests
+
+
+def get_card_image(card_name, image_url):
+    # if image directory does not exist, make one:
+    save_path = 'card_images'
+    if not (os.path.exists(save_path) and os.path.isdir(save_path)):
+        os.mkdir(save_path)
+
+    # get the image and save it:
+    response = requests.get(image_url)
+    image = response.content
+    with open(os.path.join(save_path, card_name), 'wb') as save_file:
+        save_file.write(image)
 
 
 def get_all_cards():
