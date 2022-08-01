2873d8b01a0d9bbf0bd5286f481006533f70d53e
Marcin <mlojek@protonmail.com>
2022-08-01 16:08:41 +0200

    YGOCard init: now image is the image object and not just url

--- ygo_card.py
+++ ygo_card.py
@@ -1,5 +1,5 @@
 class YGOCard:
-    def __init__(self, name, card_id, image_url):
+    def __init__(self, name, card_id, image):
         self.name = name
         self.id = card_id
-        self.image_url
+        self.image = image
