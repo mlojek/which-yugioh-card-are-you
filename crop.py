02fea920f53e84086ee320655b66943f062c52f1
Marcin <mlojek@protonmail.com>
2022-08-03 15:48:42 +0200

    add filter_canny function

--- crop.py
+++ crop.py
@@ -16,3 +16,12 @@ def random_jpg(dir_path: str) -> str:
     return random.choice([file_name for file_name in os.listdir(dir_path)
                          if os.path.isfile(os.path.join(dir_path, file_name))
                          and file_name.split('.')[-1] == 'jpg'])
+
+
+def filter_canny(image: np.ndarray) -> np.ndarray:
+    'Apply canny filter to the image'
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    blurred = cv2.GaussianBlur(gray, (3, 3), 0)
+    canny = cv2.Canny(blurred, 30, 300)
+
+    return cv2.cvtColor(canny, cv2.COLOR_GRAY2RGB)
