510b7830224f8a4b2c9c205dab9f48ecd8b233f1
Marcin <mlojek@protonmail.com>
2022-08-04 13:16:50 +0200

    add check_local_card_data function

--- wycay.py
+++ wycay.py
@@ -1,1 +1,29 @@
+import os
+import json
+
+
 CARD_DATA_DIR = 'cards'
+
+
+def check_local_card_data(dir: str) -> bool:
+    'Check the existence and validity of a local card data directory'
+    # check if the directory exists:
+    if not os.path.exists(dir) or not os.path.isdir(dir):
+        return False
+
+    # check if the cards_data.json file exists:
+    json_path = os.path.join(dir, 'cards_data.json')
+    if not os.path.exists(json_path) or not os.path.isfile(json_path):
+        return False
+
+    # read in the contents of cards_data.json:
+    cards_data = json.load(open(json_path, 'r'))
+
+    # check for every card image:
+    for card in cards_data:
+        image_name = '{}.jpg'.format(card['id'])
+        image_path = os.path.join(dir, image_name)
+        if not os.path.exists(image_path) or not os.path.isfile(image_path):
+            return False
+
+    return True
