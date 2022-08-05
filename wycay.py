ed106883748778496e33a2df7bff30b49c7f81cb
Marcin <mlojek@protonmail.com>
2022-08-05 17:38:46 +0200

    add extract_features_resnet50 function

--- wycay.py
+++ wycay.py
@@ -3,7 +3,7 @@ import shutil
 
 import cv2
 import numpy as np
-from tensorflow.keras.applications import vgg16
+from tensorflow.keras.applications import vgg16, resnet50
 
 from prodeck_api import make_local_copy, check_local_copy
 from config import CARD_DATA_DIR
@@ -29,6 +29,26 @@ def extract_features_vgg16(image: np.ndarray) -> np.ndarray:
     return model.predict(x).flatten()
 
 
+def extract_features_resnet50(image: np.ndarray) -> np.ndarray:
+    '''
+    Extract features from a given image using a pretrained resnet50 model.
+    Input shape does not matter, but the output will always be (100352,).
+    '''
+    # initialize the model:
+    model = resnet50.ResNet50(weights='imagenet', include_top=False)
+
+    # resize the image to match the model's input size:
+    img = cv2.resize(image, (224, 224), interpolation=cv2.INTER_LINEAR)
+
+    # necessary preprocessing:
+    x = np.array(img)
+    x = np.expand_dims(x, axis=0)
+    x = resnet50.preprocess_input(x)
+
+    # predict the features and return:
+    return model.predict(x).flatten()
+
+
 if __name__ == '__main__':
     # if local card data not there/not valid:
     if not check_local_copy(CARD_DATA_DIR):
@@ -39,7 +59,10 @@ if __name__ == '__main__':
         # make a local copy of card data:
         make_local_copy(CARD_DATA_DIR)
 
-    # extract features from an image:
+    # crop the card portrait:
     image = cv2.imread('cards/2511.jpg')
     cropped = image[110:435, 50:370]
-    print(extract_features_vgg16(cropped))
+
+    # extract features from the image:
+    # print(extract_features_vgg16(cropped))
+    print(extract_features_resnet50(cropped))
