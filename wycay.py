b46052add7ceeea00acb350b4b8e9bbcdbba5c85
Marcin <mlojek@protonmail.com>
2022-08-06 20:14:42 +0200

    delete extract_features_XXX functions specific to given models

--- wycay.py
+++ wycay.py
@@ -3,77 +3,22 @@ import shutil
 
 import cv2
 import numpy as np
-import pandas as pd
 from tensorflow.keras.applications import vgg16, resnet50, mobilenet
 
 from prodeck_api import make_local_copy, check_local_copy
 from config import CARD_DATA_DIR
 
 
-def extract_features_vgg16(image: np.ndarray) -> np.ndarray:
-    '''
-    Extract features from a given image using a pretrained vgg16 model.
-    Input shape does not matter, but the output will always be (25088,).
-    '''
-    # initialize the model:
-    model = vgg16.VGG16(weights='imagenet', include_top=False)
-
-    # resize the image to match the model's input size:
-    img = cv2.resize(image, (224, 224), interpolation=cv2.INTER_LINEAR)
-
-    # necessary preprocessing:
-    x = np.array(img)
-    x = np.expand_dims(x, axis=0)
-    x = vgg16.preprocess_input(x)
-
-    # predict the features and return:
-    return model.predict(x).flatten()
-
-
-def extract_features_resnet50(image: np.ndarray) -> np.ndarray:
-    '''
-    Extract features from a given image using a pretrained resnet50 model.
-    Input shape does not matter, but the output will always be (100352,).
-    '''
-    # initialize the model:
-    model = resnet50.ResNet50(weights='imagenet', include_top=False)
-
-    # resize the image to match the model's input size:
-    img = cv2.resize(image, (224, 224), interpolation=cv2.INTER_LINEAR)
-
-    # necessary preprocessing:
-    x = np.array(img)
-    x = np.expand_dims(x, axis=0)
-    x = resnet50.preprocess_input(x)
-
-    # predict the features and return:
-    return model.predict(x).flatten()
-
-
-def extract_features_mobilenet(image: np.ndarray) -> np.ndarray:
-    '''
-    Extract features from a given image using a pretrained mobilenet model.
-    Input shape does not matter, but the output will always be (50176,).
-    '''
-    # initialize the model:
-    model = mobilenet.MobileNet(weights='imagenet', include_top=False)
-
-    # resize the image to match the model's input size:
-    img = cv2.resize(image, (224, 224), interpolation=cv2.INTER_LINEAR)
-
-    # necessary preprocessing:
-    x = np.array(img)
-    x = np.expand_dims(x, axis=0)
-    x = mobilenet.preprocess_input(x)
-
-    # predict the features and return:
-    return model.predict(x).flatten()
-
-
 def extract_features(model: callable, preprocess_function: callable, image: np.ndarray) -> np.ndarray:
     '''
     Extract features from a given image using a pretrained model.
     Input shape does not matter, but the output will depend on the model.
+
+    | model     | output len |
+    |-----------|------------|
+    | vgg16     | 25088      |
+    | mobilenet | 50176      |
+    | resnet50  | 100352     |
     '''
     # initialize the model:
     net = model(weights='imagenet', include_top=False)
@@ -104,8 +49,13 @@ if __name__ == '__main__':
     image = cv2.imread('cards/2511.jpg')
     cropped = image[110:435, 50:370]
 
-    # extract features from the image:
-    # print(extract_features_vgg16(cropped))
-    # print(extract_features_resnet50(cropped))
-    # print(extract_features_mobilenet(cropped))
-    print(extract_features(mobilenet.MobileNet, mobilenet.preprocess_input, cropped))
+    # extract features from images using various models:
+    print(extract_features(vgg16.VGG16,
+                           vgg16.preprocess_input,
+                           cropped.copy()))
+    print(extract_features(resnet50.ResNet50,
+                           resnet50.preprocess_input,
+                           cropped.copy()))
+    print(extract_features(mobilenet.MobileNet,
+                           mobilenet.preprocess_input,
+                           cropped.copy()))
